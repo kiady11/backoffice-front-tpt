f41f77dfcc44ea274e238603cf8ed98eeb9134a9
rakotoharinjatovokiady <kiady.rakotoharinjatovo@yahoo.com>
2023-09-04 23:06:23 +0300

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'CoreUI Free Angular Admin Template';
+  title = 'Tableau de bord - Papier';
 
   constructor(
     private router: Router,
